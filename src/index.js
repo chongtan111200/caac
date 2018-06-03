fe7f2df4817c37c4bf616d06f11d29dd829f7e44
Chong Tian <chongtian@Chongs-MacBook-Pro.local>
2018-06-03 13:40:17 -0700

    second

--- src/index.js
+++ src/index.js
@@ -4,4 +4,28 @@ import './index.css';
 
 // create the navigation bar here
 // add a list then create the bar according to this list
-ReactDOM.render(<button />, document.getElementById('root'));
+class Nav_but extends React.Component{
+  render(){
+    return (
+      <li ><a herf='#'>{this.props.name}</a></li>
+    );
+  }
+}
+class Nav_bar extends React.Component{
+
+  render(){
+    // the navigation bar
+    var buttons = ['home','Activities','Team'];
+    return(
+      <nav class="navbar">
+      <ul class="nav navbar-nav">{
+        buttons.map((name,index) => {
+          return <Nav_but key={index} name={name}/>
+        })
+      }
+      </ul>
+      </nav>
+    );
+  }
+}
+ReactDOM.render(<Nav_bar />, document.getElementById('root'));
